ab5d22714cd0cf52d90a1f9f6d3542b1a4c472ea
Kaitlyn Gahl <kaitlyngahl@gmail.com>
2022-04-09 23:06:15 -0700

    Save before importing data and installing readr package.

--- R Scripts/Telemetry_WestClear_March.R
+++ R Scripts/Telemetry_WestClear_March.R
@@ -3,4 +3,21 @@
 #2022-4-5
 
 #Packages Used
-Rcpp Package
+library(readr)
+
+#Import Data
+
+WestClear_Telemetry_Habitat_March <- read_csv("data/raw/WestClear_Telemetry_Habitat_March.csv", 
+                                              col_types = cols(ObjectID = col_number(), 
+                                                               `Site Type` = col_character(), `Tag Number` = col_number(), 
+                                                               `Stream Width (m)` = col_number(), 
+                                                               `Other - Mesohabitat` = col_skip(), 
+                                                               `Depth (cm)` = col_number(), `Velocity (m/s)` = col_number(), 
+                                                               `Canopy Cover` = col_character(), 
+                                                               `Instream Cover Percentage (1)` = col_number(), 
+                                                               `Instream Cover Percentage (2)` = col_number(), 
+                                                               `Instream Cover Type (3)` = col_character(), 
+                                                               `Instream Cover Percentage (3)` = col_number(), 
+                                                               `Date and Time` = col_date(format = "%m/%d/%Y"), 
+                                                               `Temperature (C)` = col_number()))
+View(WestClear_Telemetry_Habitat_March)
