355f6fc1025831d3e3ef519f47066be7b49a387a
Kaitlyn Gahl <kaitlyngahl@gmail.com>
2022-04-11 23:56:49 -0700

    Made 'na' into 'NA. Removed % sign. Modified Unite code to attempt to get it to work. Planned next moves.

--- R Scripts/Telemetry_WestClear_March.R
+++ R Scripts/Telemetry_WestClear_March.R
@@ -7,20 +7,28 @@ library(readr)
 library(tidyverse)
 library(dplyr)
 
-#Import Data
-WestClear_Telemetry_Habitat_March <- read_csv("data/raw/WestClear_Telemetry_Habitat_March.csv", 
-                                              col_types = cols(ObjectID = col_number(), 
-                                                               `Site Type` = col_character(), `Tag Number` = col_number(), 
-                                                               `Stream Width (m)` = col_number(), 
-                                                               `Other - Mesohabitat` = col_skip(), 
-                                                               `Depth (cm)` = col_number(), `Velocity (m/s)` = col_number(), 
-                                                               `Canopy Cover` = col_character(), 
-                                                               `Instream Cover Percentage (1)` = col_number(), 
-                                                               `Instream Cover Percentage (2)` = col_number(), 
-                                                               `Instream Cover Type (3)` = col_character(), 
-                                                               `Instream Cover Percentage (3)` = col_number(), 
-                                                               `Date and Time` = col_date(format = "%m/%d/%Y"), 
-                                                               `Temperature (C)` = col_number()))
-View(WestClear_Telemetry_Habitat_March) #Opens window to view table 
+#Import Habitat Data
+Habitat_Data <- read_csv("data/raw/WestClear_Telemetry_Habitat_March.csv", 
+                         col_types = cols(ObjectID = col_number(), 
+                                          `Tag Number` = col_number(), `Stream Width (m)` = col_number(), 
+                                          `Other - Mesohabitat` = col_skip(), 
+                                          `Depth (cm)` = col_number(), `Velocity (m/s)` = col_number(), 
+                                          `Instream Cover Percentage (1)` = col_number(), 
+                                          `Instream Cover Percentage (2)` = col_number(), 
+                                          `Instream Cover Percentage (3)` = col_number(), 
+                                          `Date and Time` = col_date(format = "%m/%d/%Y"), 
+                                          `Temperature (C)` = col_number()))
+View(Habitat_Data)
+rename(Habitat_Data, "Tag" = "Tag Number")
+ 
+#Import Fish Data 
+Fish_Data <- read_csv("data/raw/Radio_Tagged_Fish.csv", 
+                      col_types = cols(Tag = col_number(), 
+                                       Weight = col_number(), Length = col_number(), 
+                                       `Capture Date` = col_date(format = "%m/%d/%Y"), 
+                                       Release = col_skip(), Notes = col_skip()))
+
+#Join Habitat_Data and Fish Data
+left_join(Habitat_Data, Fish_Data, by = tag)
 
 
